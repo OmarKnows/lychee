a1b2730226cda69dbd88299820972f5fe6ab80d7
OmarKnows <omarknows98@gmail.com>
2023-09-27 13:37:57 +0300

    Header Styling

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+	return (
+		<div className='flex justify-center text-primary font-bold space-x-5 py-5 [&>*]:cursor-pointer'>
+			<div>Home</div>
+			<div>About Us</div>
+			<div>The Order Process</div>
+			<div>loyalty points</div>
+			<div>Contact Us</div>
+		</div>
+	);
+};
+
+export default Header;
